94b491520386e9f5f01330b36c1b79d8d2d7ca7e
cranellbell <57388754+cranellbell@users.noreply.github.com>
2019-12-13 17:51:22 +0900

    2019-12-13
    QnA게시판 시작

---
+++ Spring_blackjack/src/main/java/www/spring/com/controller/CommonController.java
@@ -0,0 +1,16 @@
+package www.spring.com.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CommonController {
+	@GetMapping("/accessError")
+	public void accessDenied(Authentication auth, Model model) {
+		System.out.println("access Denied : " + auth);
+		
+		model.addAttribute("msg", "Acess Denied");
+	}
+}
--- Spring_blackjack/src/main/resources/www/spring/com/DDL.sql
+++ Spring_blackjack/src/main/resources/www/spring/com/DDL.sql
@@ -1,22 +1,33 @@
 drop table CUSTOMER;
 drop sequence seq_customer;
 
+drop table tbl_board;
+drop sequence seq_board;
+
+create sequence seq_board;
+
 create sequence seq_customer;
+
 create table CUSTOMER (
-	CUSTOMER_NUMBER		NUMBER(10) primary key,
-    ID              	VARCHAR2(20) not null,
-    PASSWORD        	VARCHAR2(20) not null,
-    PHONE_NUMBER    	VARCHAR2(100) not null,
-    EMAIL           	VARCHAR2(200) not null,
-    NAME           		VARCHAR2(20) not null,
-    GENDER          	VARCHAR2(10) not null,
-    CURRENT_CASH   		NUMBER(30) default 0,
-    TOTAL_AMOUNT   	    NUMBER(30) default 0,
-    WIN_RATE        	NUMBER(3) default 0,
-    REGISTRATION_DATE	date	default sysdate,
+   CUSTOMER_NUMBER      NUMBER(10) primary key,
+    ID                 VARCHAR2(20) not null,
+    PASSWORD           VARCHAR2(20) not null,
+    PHONE_NUMBER       VARCHAR2(100) not null,
+    EMAIL              VARCHAR2(200) not null,
+    NAME                 VARCHAR2(20) not null,
+    GENDER             VARCHAR2(10) not null,
+    profileImg         VARCHAR2(200),
+    CURRENT_CASH         NUMBER(30) default 0,
+    TOTAL_AMOUNT          NUMBER(30) default 0,
+    WIN_RATE           NUMBER(3) default 0,
+    REGISTRATION_DATE   date   default sysdate,
     CONSTRAINT percent check(WIN_RATE between 0 and 100)
 );
 
 insert into CUSTOMER (CUSTOMER_NUMBER, ID, PASSWORD, PHONE_NUMBER, EMAIL, NAME, GENDER, 
-	CURRENT_CASH, TOTAL_AMOUNT, WIN_RATE)
+   CURRENT_CASH, TOTAL_AMOUNT, WIN_RATE)
 values (seq_board.nextval, 'newID', 'newPassword','newPoneNum', 'newEmail', 'newName', 'male', '100', '1000', '0');
+
+
+create table tbl_board(
+);
