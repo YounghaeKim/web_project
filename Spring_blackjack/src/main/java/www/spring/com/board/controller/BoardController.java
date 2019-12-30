11160c9ff8ea9a185387a2f3ae5bb475bf90b6f6
cranellbell <57388754+cranellbell@users.noreply.github.com>
2019-12-30 17:47:46 +0900

    Revert "2019-12-27"
    This reverts commit 2b8a6153e36286e98f87cd4ecfedc21b0b4621c1.

--- Spring_blackjack/src/main/java/www/spring/com/board/controller/BoardController.java
+++ Spring_blackjack/src/main/java/www/spring/com/board/controller/BoardController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.AllArgsConstructor;
@@ -16,29 +16,20 @@ import www.spring.com.board.service.BoardService;
  *
  */
 @Controller
-//@RequestMapping("/board") // /board로 시작하는 모든 처리를 BoardController가 한다.
+@RequestMapping("/board/*") // /board로 시작하는 모든 처리를 BoardController가 한다.
 @AllArgsConstructor
 public class BoardController {
 	
-	@GetMapping("/board.do")
-	public String list(Model model) {
-		System.out.println("list");
-		
-		model.addAttribute("list", service.getList());
-		return "board/list";
-	}
-	
-	
 	private BoardService service;
 	
-	/*@GetMapping("/list")
+	@GetMapping("/list")
 	public void list(Model model) { //model을 파라미터로 지정하고 view로 넘겨준다.
 		System.out.println("list");
 		
 		model.addAttribute("list", service.getList());
-	}*/
+	}
 	
-	@PostMapping("board/register")
+	@PostMapping("/register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 		// RedirectAttributes등록 작업이 끈난 후 다시 목록 화면으로 이동
 		// 추가적으로 새롭게 등록된 게시물의 번호를 같이 전달하기 위해서 RedirectAttributes를 사용
@@ -51,32 +42,4 @@ public class BoardController {
 		return "redirect:/board/list"; //String을 리턴 타입으로 지정
 		//redirect:이 접두어는 스프링MVC가 내부적으로 response.sendRedirect()를 처리해주기때문에 편리
 	}
-	
-	@GetMapping("board/get")
-	public void get(@RequestParam("bno") Long bno, Model model) {
-		
-		System.out.println("/get");
-		model.addAttribute("board", service.get(bno));
-	}
-	
-	@PostMapping("board/modify")//수정
-	public String modify(BoardVO board, RedirectAttributes rttr) {
-		System.out.println("modify: " + board);
-		
-		if (service.modify(board)) {
-			rttr.addFlashAttribute("result", "success");
-		}
-		return "redirect:/board/list";
-	}
-	
-	@PostMapping("board/remove")
-	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
-		
-		System.out.println("remove...." + bno);
-		if (service.remove(bno)) {
-			rttr.addFlashAttribute("result","success");
-		}
-		return "redirect:/board/list";
-	}
-	
 }
